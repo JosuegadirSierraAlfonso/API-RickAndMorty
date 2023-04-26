d4e1ae62bfb6d0b3f36e3ba5ff010399e7b1ce63
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-04-26 08:20:09 -0500

    feat: :arrow_up: Search-Id
    Search characters by ID

--- components/mySearch.js
+++ components/mySearch.js
@@ -3,18 +3,27 @@ export default{
         const inputt=document.querySelector("#inputt")
         let name;
         let api;
+        let api2;
         let contenedor = document.querySelector("#resultado")
         
         const ws2 = new Worker("storage/wsMySearch.js", {type:"module"});
         inputt.addEventListener("input", (e)=>{
             name=e.target.value;
             api=`https://rickandmortyapi.com/api/character/?name=${name}`
-            ws2.postMessage(api);
+            api2=`https://rickandmortyapi.com/api/character/${name}`
+
+            if (!isNaN(+name)){
+                ws2.postMessage({message: "api2", data: api2})
+            }else{
+                ws2.postMessage({message: "api", data: api})
+            }
         })
 
         ws2.addEventListener("message", (e)=>{
           let {message, data} = e.data;
             if(message === "input"){
+                contenedor.innerHTML = data
+            }else if (message === "id"){
                 contenedor.innerHTML = data
             }
         })
--- storage/wsMySearch.js
+++ storage/wsMySearch.js
@@ -2,6 +2,7 @@
         const resp=await fetch(url)
         const result=await resp.json();
 
+
         let plantilla=""
         result.results.map((item)=>{
        
@@ -22,12 +23,47 @@
             </div>
         </div>
         `  
+
+        postMessage({message: "input", data: plantilla});
     });
     return plantilla;   
     }
 
-self.addEventListener("message", async(e) => {
-    const response=await buscarP(e.data)
+    const searchId=async(url)=>{
+        const resp=await fetch(url)
+        const result=await resp.json();
 
-    postMessage({message: "input", data: response})
-})
+        let plantilla=""
+        let item = await fetch(result.url);
+        let itemR = await item.json();
+       
+        plantilla+= `
+        <div class="card">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${itemR.image}" class="card-img-top" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title">${itemR.name}</h4>
+                        <p class="card-text"><b>Status: </b>${itemR.status}</p>
+                        <p class="card-text"><b>Specie: </b>${itemR.species}</p>
+                        <p class="card-text"><b>Gender: </b>${itemR.gender}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `  
+        postMessage({message: "id", data: plantilla});
+    return plantilla;   
+    }
+
+    onmessage = (e) =>{
+        let {message, data} = e.data;
+
+        if (message === "api"){
+            buscarP(data);
+        }else if (message === "api2"){
+            searchId(data)
+        }
+    }
