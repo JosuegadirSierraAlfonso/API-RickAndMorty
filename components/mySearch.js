e1d275d191510436379cfc06b191ee1c338cbb10
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-04-26 07:06:11 -0500

    feat: :arrow_up: Search-Name
    Search characters by name

--- components/mySearch.js
+++ components/mySearch.js
@@ -1,32 +1,22 @@
 export default{
     showSectionSearch(){
-        const container = document.querySelector("#search")
         const inputt=document.querySelector("#inputt")
         let name;
         let api;
+        let contenedor = document.querySelector("#resultado")
+        
         const ws2 = new Worker("storage/wsMySearch.js", {type:"module"});
         inputt.addEventListener("input", (e)=>{
-            console.log("eventooo");
             name=e.target.value;
             api=`https://rickandmortyapi.com/api/character/?name=${name}`
             ws2.postMessage(api);
         })
 
-        //console.log(name);
-
-        
-
-        
-
-      
-
-
         ws2.addEventListener("message", (e)=>{
-          
-
-
+          let {message, data} = e.data;
+            if(message === "input"){
+                contenedor.innerHTML = data
+            }
         })
     }
-
-
 }
--- storage/wsMySearch.js
+++ storage/wsMySearch.js
@@ -1,12 +1,33 @@
-
     const buscarP=async(url)=>{
-        console.log(url);
         const resp=await fetch(url)
         const result=await resp.json();
-        console.log(result);
+
+        let plantilla=""
+        result.results.map((item)=>{
+       
+        plantilla+= `
+        <div class="card">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${item.image}" class="card-img-top" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title">${item.name}</h4>
+                        <p class="card-text"><b>Status: </b>${item.status}</p>
+                        <p class="card-text"><b>Specie: </b>${item.species}</p>
+                        <p class="card-text"><b>Gender: </b>${item.gender}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `  
+    });
+    return plantilla;   
     }
 
 self.addEventListener("message", async(e) => {
     const response=await buscarP(e.data)
-    console.log(response);
+
+    postMessage({message: "input", data: response})
 })
