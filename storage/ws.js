48f028df76e8932a31ac656b8ffbc79bd17f6c90
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-04-25 22:54:06 -0500

    feat: :lipstick: Styles
    Add new styles

--- storage/ws.js
+++ storage/ws.js
@@ -7,16 +7,20 @@ const getData=async (apiURL) => {
     data.results.map((item)=>{
        
         plantilla+= `
-        <div class="col">
-        <div class="card" style="width: 18rem;">
-        <img src="${item.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h4 class="card-title">${item.name}</h4>
-        <p class="card-text"><b>Status: </b>${item.status}</p>
-        <p class="card-text"><b>Specie: </b>${item.species}</p>
-        <p class="card-text"><b>Gender: </b>${item.gender}</p>
-        </div>
-        </div>
+        <div class="card">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${item.image}" class="card-img-top" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title">${item.name}</h4>
+                        <p class="card-text"><b>Status: </b>${item.status}</p>
+                        <p class="card-text"><b>Specie: </b>${item.species}</p>
+                        <p class="card-text"><b>Gender: </b>${item.gender}</p>
+                    </div>
+                </div>
+            </div>
         </div>
         `
         
